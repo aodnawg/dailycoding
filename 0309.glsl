26155d2d9de11e98a7e9112a6663bb35646f91b9
unknown <mortuary.12@gmail.com>
2020-03-10 20:48:09 +0900

    0309

---
+++ 0309.glsl
@@ -0,0 +1,256 @@
+#define LOOP_MAX 1000
+#define MAX_DIST 10000.
+#define MIN_SURF .00001
+#define PI 3.141593
+
+float random(float n) {
+    return fract(sin(n*217.12312)*398.2121561);
+}
+
+float random(vec2 p) {
+    return fract(
+        sin(dot(p, vec2(98.108171, 49.10821)))*81.20914
+    );
+}
+
+float random(vec3 p) {
+    return random(random(p.xy) + p.z);
+}
+
+const mat2 myt = mat2(.12121212, .13131313, -.13131313, .12121212);
+const vec2 mys = vec2(1e4, 1e6);
+
+vec2 rhash(vec2 uv) {
+  uv *= myt;
+  uv *= mys;
+  return fract(fract(uv / mys) * uv);
+}
+
+vec3 hash(vec3 p) {
+  return fract(
+      sin(vec3(dot(p, vec3(1.0, 57.0, 113.0)), dot(p, vec3(57.0, 113.0, 1.0)),
+               dot(p, vec3(113.0, 1.0, 57.0)))) *
+      43758.5453);
+}
+
+vec3 voronoi3d(const in vec3 x) {
+  vec3 p = floor(x);
+  vec3 f = fract(x);
+
+  float id = 0.0;
+  vec2 res = vec2(100.0);
+  for (int k = -1; k <= 1; k++) {
+    for (int j = -1; j <= 1; j++) {
+      for (int i = -1; i <= 1; i++) {
+        vec3 b = vec3(float(i), float(j), float(k));
+        vec3 r = vec3(b) - f + hash(p + b);
+        float d = dot(r, r);
+
+        float cond = max(sign(res.x - d), 0.0);
+        float nCond = 1.0 - cond;
+
+        float cond2 = nCond * max(sign(res.y - d), 0.0);
+        float nCond2 = 1.0 - cond2;
+
+        id = (dot(p + b, vec3(1.0, 57.0, 113.0)) * cond) + (id * nCond);
+        res = vec2(d, res.x) * cond + res * nCond;
+
+        res.y = cond2 * d + nCond2 * res.y;
+      }
+    }
+  }
+
+  return vec3(sqrt(res), abs(id));
+}
+
+vec2 random2f(in ivec2 p) {
+    vec2 p_ = vec2(p.x, p.y);
+    float l = random(p_);
+    float m = random(l);
+    return vec2(l, m); 
+}
+
+float voronoiDistance( in vec2 x )
+{
+    ivec2 p = ivec2(floor( x ));
+    vec2  f = fract( x );
+
+    // f.x += sin(iTime+random(floor(x.x)))*.2;
+
+    ivec2 mb;
+    vec2 mr;
+
+    float res = 8.0;
+    for( int j=-1; j<=1; j++ )
+    for( int i=-1; i<=1; i++ )
+    {
+        ivec2 b = ivec2(i, j);
+        vec2  r = vec2(b) + random2f(p+b)-f;
+        float d = dot(r,r);
+
+        if( d < res )
+        {
+            res = d;
+            mr = r;
+            mb = b;
+        }
+    }
+
+    res = 8.0;
+    for( int j=-2; j<=2; j++ )
+    for( int i=-2; i<=2; i++ )
+    {
+        ivec2 b = mb + ivec2(i, j);
+        vec2  r = vec2(b) + random2f(p+b) - f;
+        float d = dot(0.5*(mr+r), normalize(r-mr));
+
+        res = min( res, d );
+    }
+
+    return res;
+}
+
+float getBorder( in vec2 p )
+{
+    float d = voronoiDistance( p );
+    return d;
+}
+
+
+float mod289(float x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
+vec4 mod289(vec4 x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
+vec4 perm(vec4 x){return mod289(((x * 34.0) + 1.0) * x);}
+float noise(vec3 p){
+    vec3 a = floor(p);
+    vec3 d = p - a;
+    d = d * d * (3.0 - 2.0 * d);
+
+    vec4 b = a.xxyy + vec4(0.0, 1.0, 0.0, 1.0);
+    vec4 k1 = perm(b.xyxy);
+    vec4 k2 = perm(k1.xyxy + b.zzww);
+
+    vec4 c = k2 + a.zzzz;
+    vec4 k3 = perm(c);
+    vec4 k4 = perm(c + 1.0);
+
+    vec4 o1 = fract(k3 * (1.0 / 41.0));
+    vec4 o2 = fract(k4 * (1.0 / 41.0));
+
+    vec4 o3 = o2 * d.z + o1 * (1.0 - d.z);
+    vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
+
+    return o4.y * d.y + o4.x * (1.0 - d.y);
+}
+float fbm(vec3 x) {
+	float v = 0.0;
+	float a = 0.5;
+	vec3 shift = vec3(100);
+	for (int i = 0; i < 7; ++i) {
+		v += a * voronoi3d(x).x;
+		x = x * 2. + shift;
+		a *= 0.5555;
+	}
+	return v;
+}
+
+
+mat2 rot(float a) {
+    float c = cos(a), s = sin(a);
+    return mat2(c,s,-s,c);
+}
+
+vec2 pmod(vec2 p, float r) {
+    float a =  atan(p.x, p.y) + PI/r;
+    float n = PI*2. / r;
+    a = floor(a/n)*n;
+    return p*rot(-a);
+}
+
+float sdBox( in vec2 p, in vec2 b )
+{
+    vec2 d = abs(p)-b;
+    return length(max(d,0.0)) + min(max(d.x,d.y),0.0);
+}
+
+float map(vec3 p, vec3 ro) {
+    // p.x -= 10.;
+    // p.z -= 15.;
+    // p = mod(p, 10.)-5.;
+    // float id = random(floor(p.xz/20.));
+
+    p.xz *= rot(iTime);
+    p.xz *= rot(p.y*.1);
+    // return length(p) - 1.;
+    // p.xy *= rot(p.z);
+    float g = getBorder(p.yx*(.5+sin(iTime*.3)*2.))-.05+sin(iTime*10.)*.02;
+    float mask = sdBox(p.xz, vec2(50.,.0001))-.5;
+    return max(g, mask);
+}
+
+struct Trace {
+    float d; bool isHit; float s;
+};
+Trace trace(vec3 ro, vec3 rd) {
+    Trace mr;
+    float t = 0.;
+    float s = 0.;
+    bool flag;
+    for(int i=0; i<LOOP_MAX; i++) {
+        vec3 p = ro+rd*t;
+        float d = map(p, ro);
+        if(d<MIN_SURF) {
+            flag=true;
+            break;
+        }
+        if(t>MAX_DIST) {
+            break;
+        }
+        flag=false;
+        t += d;
+        s += 1./200.;
+    }
+    mr.d = t;
+    mr.s = s;
+    mr.isHit = flag;
+    return mr;
+}
+
+struct Camera {
+    vec3 ro; vec3 rd; float z;
+};
+Camera makeCam(in vec2 uv, float s) {
+    Camera camera;
+    float n = noise(vec3(1.,1.,s));
+    vec3 ro = vec3(0) - vec3(0,4.,-3);
+    ro += vec3(0,s,0);
+    vec3 lookat = vec3(0)+vec3(0,s,0);
+    vec3 f = normalize(lookat-ro);
+    vec3 r = cross(vec3(0,1,0), f);
+    vec3 u = cross(f, r);
+    float z = .4;
+    vec3 c = ro+f*z;
+    vec3 i = c+r*uv.x+u*uv.y;
+    vec3 rd = normalize(i-ro);
+    camera.ro = ro;
+    camera.rd = rd;
+    camera.z = z;
+    return camera;
+}
+
+
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+    vec3 col = vec3(0.);
+    vec2 uv = (fragCoord.xy - iResolution.xy*.5)/iResolution.y;
+    float s = iTime;
+    Camera c = makeCam(uv, s);
+    Trace t = trace(c.ro, c.rd);
+    float w = mix(.01,.02, sin(iTime)*.5+.5)*2.;
+    col = vec3(pow(t.s,1.)*(1.+t.d*w));
+
+ 
+    // uv *= 1. + noise(vec3(uv*3., 1.))*2.;
+    // col = vec3(getBorder(uv*10.));
+
+    fragColor = vec4(col, 1.);
+}
