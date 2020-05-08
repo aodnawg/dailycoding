8cd8fde25761db3fd20fb6f78ef9c7bf7b4d0db4
unknown <mortuary.12@gmail.com>
2020-05-09 00:09:30 +0900

    ass getMetaFromComment

--- script/makeMovie.js
+++ script/makeMovie.js
@@ -1,55 +1,76 @@
+// @ts-check
+
 const fs = require("fs");
 const path = require("path");
 const assert = require("assert");
 const child_process = require("child_process");
 
-const encoding = "utf-8";
-const shaderDirPath = path.resolve(__dirname, "..", "shaders");
-const processingSketchPath = path.resolve(__dirname, "..", "Shader");
-const processingDataPath = path.resolve(processingSketchPath, "data");
-/** shader reffered in processing */
-const sourceShaderName = "frag.glsl";
-/** rendering config loaded processing */
-const settingJsonName = "setting.json";
-/** frame image sequence path */
-const sequenceImagePath = path.resolve(processingSketchPath, "sequence");
-/** output movie destination */
-const outputPath = path.resolve(__dirname, "..", "out");
+const {
+  pathData: {
+    shaderSourcePath,
+    processingSketchPath,
+    processingDataPath,
+    processingSourceShaderName,
+    processingSettingJsonName,
+    sequenceImagePath,
+    movieOutputPath,
+  },
+  encoding,
+  getNewestShaderFileName,
+} = require("./common");
+
+const getMetaFromComment = require("./getMetaFromComment");
 
 const getTimestamp = () => new Date().getTime();
 
-const checkTargetShader = (targetShaderName) => {
-  const result = fs.existsSync(path.resolve(shaderDirPath, targetShaderName));
-  assert(result, `[*] ${targetShaderName} does not exist.`);
-  return targetShaderName;
+/**
+ * @param {string} targetShaderPath
+ */
+const checkTargetShader = (targetShaderPath) => {
+  const result = fs.existsSync(targetShaderPath);
+  assert(result, `[*] ${targetShaderPath} does not exist.`);
+  return targetShaderPath;
 };
 
+/**
+ * @param {string} targetShaderName
+ */
 const copyTargetShader = (targetShaderName) => {
-  const destinationPath = path.resolve(processingDataPath, sourceShaderName);
+  const destinationPath = path.resolve(
+    processingDataPath,
+    processingSourceShaderName
+  );
   if (fs.existsSync(destinationPath)) {
     fs.unlinkSync(destinationPath);
     console.log("[*] removed old shader.");
   }
   fs.copyFileSync(
-    path.resolve(shaderDirPath, targetShaderName),
+    path.resolve(shaderSourcePath, targetShaderName),
     destinationPath
   );
 };
 
+/**
+ * @param {string} timestamp
+ * @return {string} json
+ */
 const makeSettingJson = (timestamp) => {
   const map = {
     timestamp,
     width: 1000,
     height: 1000,
-    frameRate: 24,
-    totalTime: 30,
-    shader: sourceShaderName,
+    frameRate: 4,
+    totalTime: 1,
+    shader: processingSourceShaderName,
   };
   return JSON.stringify(map);
 };
 
+/**
+ * @param {string} jsonString
+ */
 const inputSettingJson = (jsonString) => {
-  const jsonPath = path.resolve(processingDataPath, settingJsonName);
+  const jsonPath = path.resolve(processingDataPath, processingSettingJsonName);
   if (fs.existsSync(jsonPath)) {
     fs.unlinkSync(jsonPath);
     console.log("[*] removed old setting.");
@@ -58,24 +79,25 @@ const inputSettingJson = (jsonString) => {
   console.log("[*] input rendering option.");
 };
 
-const getNewestShaderFile = () =>
-  fs
-    .readdirSync(shaderDirPath)
-    .sort(
-      (a, b) =>
-        fs.statSync(path.resolve(shaderDirPath, b)).mtime.getTime() -
-        fs.statSync(path.resolve(shaderDirPath, a)).mtime.getTime()
-    )[0];
-
+/**
+ * @typedef {Object} SetupedParam
+ * @property {string} timestamp
+ * @property {string} targetShaderPath
+ * @property {string} frameRate
+ */
+/**
+ * @return {SetupedParam}
+ */
 const setup = () => {
-  const targetShaderName = process.argv[2] || getNewestShaderFile();
-  console.log(`[*] setup ${targetShaderName}`);
-  checkTargetShader(targetShaderName);
+  const targetShaderName = process.argv[2] || getNewestShaderFileName();
+  const targetShaderPath = path.resolve(shaderSourcePath, targetShaderName);
+  console.log(`[*] setup ${targetShaderPath}`);
+  checkTargetShader(targetShaderPath);
   copyTargetShader(targetShaderName);
   const timestamp = `${getTimestamp()}`;
   const jsonString = makeSettingJson(timestamp);
   inputSettingJson(jsonString);
-  return { timestamp };
+  return { timestamp, targetShaderPath, frameRate: "24" }; //TODO: import frameRate
 };
 
 const execProcessing = () => {
@@ -86,23 +108,45 @@ const execProcessing = () => {
   return true;
 };
 
-const execEncoding = (timestamp) => {
+/**
+ *
+ * @typedef {Object} FfmpegParams - parameters required for ffmpeg
+ * @property {string} timestamp
+ * @property {string} frameRate
+ */
+/**
+ * @param {FfmpegParams} ffmpegParams
+ */
+const execEncoding = ({ timestamp, frameRate }) => {
   console.log("[*] exec movie encoding");
   const sourcePath = path.resolve(sequenceImagePath, `${timestamp}`);
-  const movieFileName = path.resolve(outputPath, `${timestamp}.mp4`);
-  const cmd = `ffmpeg -s 1000x1000 -framerate 24 -i ${sourcePath}\\%04d.tif -vcodec libx264 -pix_fmt yuv420p ${movieFileName}`;
+  const movieFileName = path.resolve(movieOutputPath, `${timestamp}.mp4`);
+  const cmd = `ffmpeg -s 1000x1000 -framerate ${frameRate} -i ${sourcePath}\\%04d.tif -vcodec libx264 -pix_fmt yuv420p ${movieFileName}`;
   child_process.execSync(cmd);
   console.log("[*] complete movie encoding");
   return true;
 };
 
+/**
+ * @param {string} targetShaderName
+ */
+const dumpMetaInfo = (targetShaderName) => {
+  const { day, tag, title: titleText } = getMetaFromComment(targetShaderName);
+  const dayText = day && `Day ${day}`;
+  const tagText = tag && `${tag.map((t) => `#${t}`).join(" ")}`;
+  return [dayText, titleText, tagText].filter((t) => !!t).join("\n\n");
+};
+
 const main = () => {
   // setup
-  const { timestamp, frameRate } = setup();
+  const { timestamp, frameRate, targetShaderPath } = setup();
   // exec
   execProcessing();
   // encoding
-  execEncoding(timestamp, frameRate);
+  execEncoding({ timestamp, frameRate });
+  // meta
+  const result = dumpMetaInfo(targetShaderPath);
+  console.log(result);
   console.log("[*] done ✨");
 };
 
